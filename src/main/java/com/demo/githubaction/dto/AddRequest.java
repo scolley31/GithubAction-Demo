2babaf53757b5bd3d283850d5da81b3d15b446db
scolley <scolley.huang@kkday.com>
2024-04-13 21:17:28 +0800

    tmp

--- src/main/java/com/demo/githubaction/dto/AddRequest.java
+++ src/main/java/com/demo/githubaction/dto/AddRequest.java
@@ -9,8 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddRequest
-                {
-    private int a
+public class AddRequest {
+    private int a;
     private int b;
 }
